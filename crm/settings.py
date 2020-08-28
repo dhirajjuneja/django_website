68113d4c1a49f4d650327eb4bd96e8bc12487e38
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-08-28 16:26:25 +0530

    some error fixed

--- crm/settings.py
+++ crm/settings.py
@@ -78,14 +78,11 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DEMO_TEST',
-        'USER': 'postgres',
-        'PASSWORD': 'dhiraj22',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation

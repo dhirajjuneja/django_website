07eaa21cb5f1b7fa00ad9764ac5ec93510c71247
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-08-28 16:45:06 +0530

    master

--- crm/settings.py
+++ crm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'gh8ie=0#p2=*-p+5)nk5nsyb($$^j+4tz)nm-_zt9zq=!sb^(w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://goall--diggers2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition

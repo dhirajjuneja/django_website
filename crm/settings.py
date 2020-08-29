d2967096123e08ae1e99f95a4fec1ad4c50eee18
Rahul juneja <63409229+Rahuljuneja22@users.noreply.github.com>
2020-08-29 11:28:23 +0530

    master

--- crm/settings.py
+++ crm/settings.py
@@ -85,7 +85,9 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+dj_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(dj_from_env)
 
 
 # Password validation
